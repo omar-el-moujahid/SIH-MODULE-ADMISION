4af5ca2d39f85a5a04c5101e22baf4507cd679d2
omar <oamrelmoujahid5@gmail.com>
2024-07-11 19:04:25 +0100

    ferst commit

---
+++ src/main/java/ma/ensa/sihmoduleadmission/SihModuleAdmissionApplication.java
@@ -0,0 +1,13 @@
+package ma.ensa.sihmoduleadmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SihModuleAdmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SihModuleAdmissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/ensa/sihmoduleadmission/SihModuleAdmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensa.sihmoduleadmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SihModuleAdmissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
